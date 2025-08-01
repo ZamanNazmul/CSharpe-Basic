e62e1e8e620a212e848eeda6ba9ceeefad18a0f2
Nazmul Zaman <95521785+ZamanNazmul@users.noreply.github.com>
2025-08-01 22:16:09 +0600

    Create ComparisionOp.cs

---
+++ ComparisionOp.cs
@@ -0,0 +1,21 @@
+
+using System;
+class Testb
+{
+    public static void Main(string[] args)
+    {
+        //Comparision operators 
+
+        int number1 = 20;
+        int number2 = 10;
+
+        Console.WriteLine(number1 > number2);
+        Console.WriteLine(number1 >= number2);
+        Console.WriteLine(number1 < number2);
+        Console.WriteLine(number1 <= number2);
+        Console.WriteLine(number1 == number2);
+        Console.WriteLine(number1 != number2);
+        
+
+    }
+}
