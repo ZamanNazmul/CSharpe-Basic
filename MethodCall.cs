155701291bb5bdf1344e95559cf7e5f292ac5389
Nazmul Zaman <95521785+ZamanNazmul@users.noreply.github.com>
2025-08-08 00:53:16 +0600

    Create MethodCall.cs

---
+++ MethodCall.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    //Method Call
+    public static void Add()
+    {
+        Console.Write("My Name is: Nazmul Zaman");
+   
+    }
+    public static void Main(string[] args)
+    {
+        Add();
+        Console.Read();
+    }
+}
