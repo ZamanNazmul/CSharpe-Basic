7839e431f11dd3d36305023cda690567e8b479d8
Nazmul Zaman <95521785+ZamanNazmul@users.noreply.github.com>
2025-08-08 00:31:54 +0600

    Create DoWhile.cs

---
+++ DoWhile.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        // Do While Loop
+        int i = 1;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        while (i <= 10);  // Continue as long as i is less than or equal to 10
+    }
+}
