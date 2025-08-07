0d9982f93d7aeb7d13e46024a2a349fe18b13968
Nazmul Zaman <95521785+ZamanNazmul@users.noreply.github.com>
2025-08-08 00:31:30 +0600

    Create whileLoop.cs

---
+++ whileLoop.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        // While Loop
+        int i = 10;
+        while (i <= 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
