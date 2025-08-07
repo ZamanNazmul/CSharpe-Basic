2fe1f344bfdc2cd2a7195420b6557e2b80768c3b
Nazmul Zaman <95521785+ZamanNazmul@users.noreply.github.com>
2025-08-08 00:32:50 +0600

    Create BreakContinue2.cs

---
+++ BreakContinue2.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        // Break and Continue statement.
+
+        // It will print 1 to 99 and skipped 49 , if  i = 100 , it will break; 
+        for (int i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.WriteLine($"i = {i},  j = {j} = CSharpe ");
+            }
+        }
+    }
+}
